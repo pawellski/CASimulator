123320f149c36d28b3f8660cb095616528f04695
Paweł <Pawe@DESKTOP-U6NIHLO>
2019-05-28 11:52:09 +0200

    Dodanie klasy Grid

---
+++ CASimulator/src/Grid/Grid.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Grid;
+
+/**
+ *
+ * @author Paweł
+ */
+public class Grid {
+
+    private int width;
+    private int height;
+    public Cell[][] gameGrid;
+
+    public Grid(int hei, int wi) {
+        height = hei;
+        width = wi;
+        gameGrid = new Cell[height][width];
+    }
+
+    public void resize(int hei, int wi) {
+        height = hei;
+        width = wi;
+        gameGrid = new Cell[height][width];
+    }
+
+}
