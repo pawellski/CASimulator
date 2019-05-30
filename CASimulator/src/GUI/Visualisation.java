0638a0ae7f585f04681386aacbab5b4c70f2cf31
Paweł <Pawe@DESKTOP-U6NIHLO>
2019-05-30 21:24:46 +0200

    Poprawa funkcji onUpdate();

--- CASimulator/src/GUI/Visualisation.java
+++ CASimulator/src/GUI/Visualisation.java
@@ -6,6 +6,7 @@
 package GUI;
 
 import Core.CellularAutomaton;
+import Core.GameOfLife;
 import Core.WireWorld;
 import Grid.Cell;
 import java.awt.Color;
@@ -18,46 +19,41 @@ import java.awt.Image;
  */
 public class Visualisation extends javax.swing.JFrame implements Observator {
 
-    private int numberHorizontalCalls = 15;
-    private int numberVerticalCalls = 10;
+    private int numberHorizontalCells = 15;
+    private int numberVerticalCells = 10;
 
     private int widthCell;
     private int heightCell;
 
     private char typeOfGame = 'W';
-    private CellularAutomaton currentGame = new WireWorld(numberVerticalCalls + 2, numberHorizontalCalls + 2);
+    private CellularAutomaton currentGame = new WireWorld(numberVerticalCells + 2, numberHorizontalCells + 2);
 
     Graphics paintGrid;
     Image mainImage;
 
     public Visualisation() {
         initComponents();
-        System.out.println(currentGame.getClass().getName());
+        mainImage = createImage(mainPanel.getWidth(), mainPanel.getHeight());
+        paintGrid = mainImage.getGraphics();
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                onUpdate();
+            }
+        }
+        );
     }
 
+    @Override
     public void onUpdate() {
-        widthCell = mainPanel.getWidth() / numberHorizontalCalls;
-        heightCell = mainPanel.getHeight() / numberVerticalCalls;
+        widthCell = mainPanel.getWidth() / numberHorizontalCells;
+        heightCell = mainPanel.getHeight() / numberVerticalCells;
         paintGrid.setColor(mainPanel.getForeground());
         paintGrid.fillRect(0, 0, mainPanel.getWidth(), mainPanel.getHeight());
 
         switch (currentGame.getClass().getName()) {
             case "Core.WireWorld":
-                for (int i = 0; i < numberVerticalCalls; i++) {
-                    for (int j = 0; j < numberHorizontalCalls; j++) {
-                        if (currentGame.getCellFromGrid(i + 1, j + 1) == Cell.ALIVE) {
-                            paintGrid.setColor(Color.WHITE);
-                            paintGrid.fillRect(j * heightCell, i * widthCell, widthCell, heightCell);
-                        } else {
-                            paintGrid.setColor(Color.BLACK);
-                            paintGrid.fillRect(j * heightCell, i * widthCell, widthCell, heightCell);
-                        }
-                    }
-                }
-                break;
-            case "Core.GameOfLife":
-                for (int i = 0; i < numberVerticalCalls; i++) {
-                    for (int j = 0; j < numberHorizontalCalls; j++) {
+                for (int i = 0; i < numberVerticalCells; i++) {
+                    for (int j = 0; j < numberHorizontalCells; j++) {
                         switch (currentGame.getCellFromGrid(i + 1, j + 1)) {
                             case EMPTY:
                                 paintGrid.setColor(Color.BLACK);
@@ -79,10 +75,29 @@ public class Visualisation extends javax.swing.JFrame implements Observator {
                     }
                 }
                 break;
+            case "Core.GameOfLife":
+                for (int i = 0; i < numberVerticalCells; i++) {
+                    for (int j = 0; j < numberHorizontalCells; j++) {
+                        if (currentGame.getCellFromGrid(i + 1, j + 1) == Cell.ALIVE) {
+                            paintGrid.setColor(Color.WHITE);
+                            paintGrid.fillRect(j * heightCell, i * widthCell, widthCell, heightCell);
+                        } else {
+                            paintGrid.setColor(Color.BLACK);
+                            paintGrid.fillRect(j * heightCell, i * widthCell, widthCell, heightCell);
+                        }
+                    }
+                }
+                break;
+        }
+        paintGrid.setColor(Color.WHITE);
+        for (int i = 0; i < mainPanel.getHeight() + heightCell; i += heightCell) {
+            paintGrid.drawLine(0, i, mainPanel.getWidth(), i);
+        }
+        for (int i = 0; i < mainPanel.getWidth() + widthCell; i += widthCell) {
+            paintGrid.drawLine(i, 0, i, mainPanel.getHeight());
         }
 
         mainPanel.getGraphics().drawImage(mainImage, 0, 0, mainPanel);
-        System.out.println(mainPanel.getHeight() + " xxxxx " + mainPanel.getWidth());
     }
 
     /**
@@ -118,9 +133,9 @@ public class Visualisation extends javax.swing.JFrame implements Observator {
         jRadioButtonWire = new javax.swing.JRadioButton();
         jRadioButtonElectronHead = new javax.swing.JRadioButton();
         jRadioButtonElectronTail = new javax.swing.JRadioButton();
-        jPanel1 = new javax.swing.JPanel();
-        jPanel2 = new javax.swing.JPanel();
-        jPanel3 = new javax.swing.JPanel();
+        jPanelYellow = new javax.swing.JPanel();
+        jPanelBlue = new javax.swing.JPanel();
+        jPanelRed = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("CA Simulator");
@@ -147,6 +162,11 @@ public class Visualisation extends javax.swing.JFrame implements Observator {
         jToggleButtonGoLWW.setActionCommand("");
         jToggleButtonGoLWW.setDoubleBuffered(true);
         jToggleButtonGoLWW.setPreferredSize(new java.awt.Dimension(150, 25));
+        jToggleButtonGoLWW.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jToggleButtonGoLWWMouseClicked(evt);
+            }
+        });
 
         jLabelNumberGeneration.setFont(new java.awt.Font("Verdana", 1, 12)); // NOI18N
         jLabelNumberGeneration.setText("Number of generations");
@@ -227,45 +247,45 @@ public class Visualisation extends javax.swing.JFrame implements Observator {
         jRadioButtonElectronTail.setText("Electron Tail");
         jRadioButtonElectronTail.setPreferredSize(new java.awt.Dimension(107, 20));
 
-        jPanel1.setBackground(new java.awt.Color(255, 255, 102));
-        jPanel1.setPreferredSize(new java.awt.Dimension(20, 20));
+        jPanelYellow.setBackground(new java.awt.Color(255, 255, 102));
+        jPanelYellow.setPreferredSize(new java.awt.Dimension(20, 20));
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout jPanelYellowLayout = new javax.swing.GroupLayout(jPanelYellow);
+        jPanelYellow.setLayout(jPanelYellowLayout);
+        jPanelYellowLayout.setHorizontalGroup(
+            jPanelYellowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 20, Short.MAX_VALUE)
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 20, Short.MAX_VALUE)
-        );
-
-        jPanel2.setBackground(new java.awt.Color(0, 51, 204));
-        jPanel2.setPreferredSize(new java.awt.Dimension(20, 20));
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 20, Short.MAX_VALUE)
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        jPanelYellowLayout.setVerticalGroup(
+            jPanelYellowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 20, Short.MAX_VALUE)
         );
 
-        jPanel3.setBackground(new java.awt.Color(255, 51, 51));
-        jPanel3.setPreferredSize(new java.awt.Dimension(20, 20));
+        jPanelBlue.setBackground(new java.awt.Color(0, 51, 204));
+        jPanelBlue.setPreferredSize(new java.awt.Dimension(20, 20));
 
-        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-        jPanel3.setLayout(jPanel3Layout);
-        jPanel3Layout.setHorizontalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout jPanelBlueLayout = new javax.swing.GroupLayout(jPanelBlue);
+        jPanelBlue.setLayout(jPanelBlueLayout);
+        jPanelBlueLayout.setHorizontalGroup(
+            jPanelBlueLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 20, Short.MAX_VALUE)
         );
-        jPanel3Layout.setVerticalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        jPanelBlueLayout.setVerticalGroup(
+            jPanelBlueLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 20, Short.MAX_VALUE)
+        );
+
+        jPanelRed.setBackground(new java.awt.Color(255, 51, 51));
+        jPanelRed.setPreferredSize(new java.awt.Dimension(20, 20));
+
+        javax.swing.GroupLayout jPanelRedLayout = new javax.swing.GroupLayout(jPanelRed);
+        jPanelRed.setLayout(jPanelRedLayout);
+        jPanelRedLayout.setHorizontalGroup(
+            jPanelRedLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 20, Short.MAX_VALUE)
+        );
+        jPanelRedLayout.setVerticalGroup(
+            jPanelRedLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 20, Short.MAX_VALUE)
         );
 
@@ -293,15 +313,15 @@ public class Visualisation extends javax.swing.JFrame implements Observator {
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jRadioButtonWire, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jPanelYellow, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(jRadioButtonElectronHead, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jPanelBlue, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(jRadioButtonElectronTail, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addComponent(jPanelRed, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                         .addGap(28, 28, 28))))
             .addGroup(layout.createSequentialGroup()
                 .addGap(620, 620, 620)
@@ -391,9 +411,9 @@ public class Visualisation extends javax.swing.JFrame implements Observator {
                                 .addComponent(jRadioButtonElectronHead, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(jRadioButtonWire))
                             .addComponent(jRadioButtonElectronTail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addComponent(jPanelYellow, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jPanelBlue, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jPanelRed, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addGap(33, 41, Short.MAX_VALUE))
         );
 
@@ -409,6 +429,31 @@ public class Visualisation extends javax.swing.JFrame implements Observator {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jToggleButtonGoLWWMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jToggleButtonGoLWWMouseClicked
+        switch (currentGame.getClass().getName()) {
+            case "Core.WireWorld":
+                currentGame = new GameOfLife(numberVerticalCells + 2, numberHorizontalCells + 2);
+                jToggleButtonGoLWW.setText("GAME OF LIFE");
+                jRadioButtonWire.setVisible(false);
+                jRadioButtonElectronHead.setVisible(false);
+                jRadioButtonElectronTail.setVisible(false);
+                jPanelYellow.setVisible(false);
+                jPanelBlue.setVisible(false);
+                jPanelRed.setVisible(false);
+                break;
+            case "Core.GameOfLife":
+                currentGame = new WireWorld(numberVerticalCells + 2, numberHorizontalCells + 2);
+                jToggleButtonGoLWW.setText("WIRE WORLD");
+                jRadioButtonWire.setVisible(true);
+                jRadioButtonElectronHead.setVisible(true);
+                jRadioButtonElectronTail.setVisible(true);
+                jPanelYellow.setVisible(true);
+                jPanelBlue.setVisible(true);
+                jPanelRed.setVisible(true);
+                break;
+        }
+    }//GEN-LAST:event_jToggleButtonGoLWWMouseClicked
 
     /**
      * @param args the command line arguments
@@ -462,9 +507,9 @@ public class Visualisation extends javax.swing.JFrame implements Observator {
     private javax.swing.JLabel jLabelGridSize;
     private javax.swing.JLabel jLabelInterval;
     private javax.swing.JLabel jLabelNumberGeneration;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanelBlue;
+    private javax.swing.JPanel jPanelRed;
+    private javax.swing.JPanel jPanelYellow;
     private javax.swing.JRadioButton jRadioButtonElectronHead;
     private javax.swing.JRadioButton jRadioButtonElectronTail;
     private javax.swing.JRadioButton jRadioButtonWire;
