ba29335fa7033727680c87181ef6e3d0f074b313
Paweł <Pawe@DESKTOP-U6NIHLO>
2019-06-03 02:00:08 +0200

    Dostosowanie klasy CA pod get/set klasy Grid.

--- CASimulator/src/Core/CellularAutomaton.java
+++ CASimulator/src/Core/CellularAutomaton.java
@@ -26,6 +26,15 @@ public abstract class CellularAutomaton implements Observable {
         observers.add(o);
     }
 
+    public Grid getMainGrid() {
+        return mainGrid;
+    }
+
+    public void setMainGrid(Grid mainGrid) {
+        this.mainGrid = mainGrid;
+    }
+    
+
     @Override
     public void removeObservator(Observator o) {
         int index = observers.indexOf(o);
@@ -42,37 +51,37 @@ public abstract class CellularAutomaton implements Observable {
     public CellularAutomaton(int hei, int wi) {
         mainGrid = new Grid(hei, wi);
         utilGrid = new Grid(hei, wi);
-        clearGrid(mainGrid.gameGrid);
+        clearGrid(mainGrid.getGameGrid());
         observers = new ArrayList<Observator>();
     }
 
     public CellularAutomaton(Cell[][] grid) {
         mainGrid = new Grid();
-        mainGrid.gameGrid = grid;
-        utilGrid = new Grid(mainGrid.gameGrid.length, mainGrid.gameGrid[0].length);
+        mainGrid.setGameGrid(grid);
+        utilGrid = new Grid(mainGrid.getHeight(), mainGrid.getWidth());
         observers = new ArrayList<Observator>();
     }
 
-    public Cell getCellFromGrid(int i, int j) {
+    /*public Cell getCellFromGrid(int i, int j) {
         return mainGrid.gameGrid[i][j];
     }
 
     public void setCellFromGrid(int i, int j, Cell state) {
         this.mainGrid.gameGrid[i][j] = state;
-    }
+    }*/
 
     public void changeGrid() {
-        for (int i = 0; i < mainGrid.gameGrid.length; i++) {
-            for (int j = 0; j < mainGrid.gameGrid[i].length; j++) {
-                mainGrid.gameGrid[i][j] = utilGrid.gameGrid[i][j];
+        for (int i = 0; i < mainGrid.getHeight(); i++) {
+            for (int j = 0; j < mainGrid.getWidth(); j++) {
+                mainGrid.setGameGridCell(i, j, utilGrid.getGameGridCell(i, j));
             }
         }
     }
 
     protected void printToScreen() {
-        for (int i = 1; i < mainGrid.gameGrid.length - 1; i++) {
-            for (int j = 1; j < mainGrid.gameGrid[i].length - 1; j++) {
-                System.out.print(mainGrid.gameGrid[i][j] + " ");
+        for (int i = 1; i < mainGrid.getHeight() - 1; i++) {
+            for (int j = 1; j < mainGrid.getWidth() - 1; j++) {
+                System.out.print(mainGrid.getGameGridCell(i, j) + " ");
             }
             System.out.println();
         }
