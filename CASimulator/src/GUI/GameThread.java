47999c631c9c326612d365f69fb9663aa571100c
Paweł <Pawe@DESKTOP-U6NIHLO>
2019-06-07 20:53:17 +0200

    Dodanie nowej klasy dziedziczącej po Thread.

---
+++ CASimulator/src/GUI/GameThread.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import Core.CellularAutomaton;
+
+/**
+ *
+ * @author Paweł
+ */
+public class GameThread extends Thread {
+
+    private boolean flag = true;
+    private CellularAutomaton threadAutomaton;
+    private int countGenerations;
+    private int intervalTime;
+
+    public GameThread(CellularAutomaton game, int generations, int interval) {
+        this.threadAutomaton = game;
+        this.countGenerations = generations;
+        this.intervalTime = interval;
+    }
+
+    @Override
+    public void run() {
+        int iterations = 0;
+        while (flag && iterations < countGenerations) {
+            threadAutomaton.generate();
+            try {
+                Thread.sleep(intervalTime);
+            } catch (InterruptedException ex) {
+
+            }
+            iterations++;
+        }
+    }
+
+    @Override
+    public void interrupt() {
+        flag = false;
+    }
+
+}
