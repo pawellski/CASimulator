54d17a0dc6d9133759efe379b5f60f2679cf0094
Paweł <Pawe@DESKTOP-U6NIHLO>
2019-06-07 20:52:21 +0200

    Poprawa funkcji generate()

--- CASimulator/src/Core/GameOfLife.java
+++ CASimulator/src/Core/GameOfLife.java
@@ -11,10 +11,10 @@ import Grid.Cell;
  *
  * @author Jakub
  */
-public class GameOfLife extends CellularAutomaton{
+public class GameOfLife extends CellularAutomaton {
 
-    public GameOfLife(int wi, int hei) {
-        super(wi, hei);
+    public GameOfLife(int hei, int wi) {
+        super(hei, wi);
     }
 
     public GameOfLife(Cell[][] grid) {
@@ -22,30 +22,25 @@ public class GameOfLife extends CellularAutomaton{
     }
 
     @Override
-    public void generate(int intervalTime) {
-        //for (int it = 0; it < generationCount; it++) {
-            for (int i = 1; i < mainGrid.getHeight() - 1; i++) {
-                for (int j = 1; j < mainGrid.getWidth() - 1; j++) {
-                    utilGrid.setGameGridCell(i, j, cellState(i, j));
-                }
+    public void generate() {
+        for (int i = 1; i < mainGrid.getHeight() - 1; i++) {
+            for (int j = 1; j < mainGrid.getWidth() - 1; j++) {
+                this.utilGrid.setGameGridCell(i, j, cellState(i, j));
             }
-            notifyObservator();
-           // try {
-              //  Thread.sleep(intervalTime);
-           // } catch (InterruptedException e) {
-           // }
-            clearGrid(mainGrid.getGameGrid());
-            changeGrid();
-            clearGrid(utilGrid.getGameGrid());
         }
-   // }
+        notifyObservator();
+        printToScreen();
+        clearGrid(mainGrid.getGameGrid());
+        changeGrid();
+        clearGrid(utilGrid.getGameGrid());
+    }
 
     @Override
     public Cell cellState(int i, int j) {
         int aliveNumber = 0;
         for (int a = -1; a < 2; a++) {
             for (int b = -1; b < 2; b++) {
-                if (mainGrid.getGameGridCell(i+a, j+b) == Cell.ALIVE) {
+                if (mainGrid.getGameGridCell(i + a, j + b) == Cell.ALIVE) {
                     aliveNumber++;
                 }
             }
@@ -65,7 +60,7 @@ public class GameOfLife extends CellularAutomaton{
             }
         }
     }
-   
+
     @Override
     public void clearGrid(Cell[][] gridToClear) {
         for (int i = 0; i < gridToClear.length; i++) {
@@ -74,5 +69,5 @@ public class GameOfLife extends CellularAutomaton{
             }
         }
     }
-    
+
 }
