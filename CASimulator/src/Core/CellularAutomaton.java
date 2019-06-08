1f51304904c53f252e2976e7f6265523fd5bfe2b
Paweł <Pawe@DESKTOP-U6NIHLO>
2019-06-08 22:16:14 +0200

    Usunięcie niepotrzebnych konstruktorów, metody printToScreen oraz getterów/setterów w klasie CellularAutomaton

--- CASimulator/src/Core/CellularAutomaton.java
+++ CASimulator/src/Core/CellularAutomaton.java
@@ -6,8 +6,7 @@
 package Core;
 
 import GUI.Observator;
-import Grid.Cell;
-import Grid.Grid;
+import Grid.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -23,21 +22,16 @@ public abstract class CellularAutomaton implements Observable {
 
     private ArrayList<Observator> observers;
 
+    public CellularAutomaton(int hei, int wi) {
+        mainGrid = new Grid(hei, wi);
+        utilGrid = new Grid(hei, wi);
+        clearGrid(mainGrid.getGameGrid());
+        observers = new ArrayList<Observator>();
+    }
+
     @Override
     public void addObservator(Observator o) {
         observers.add(o);
-    }
-
-    public Grid getMainGrid() {
-        return mainGrid;
-    }
-    
-    public Grid getUtilGrid() {
-        return utilGrid;
-    }
-
-    public void setMainGrid(Grid mainGrid) {
-        this.mainGrid = mainGrid;
     }
 
     @Override
@@ -53,20 +47,6 @@ public abstract class CellularAutomaton implements Observable {
         }
     }
 
-    public CellularAutomaton(int hei, int wi) {
-        mainGrid = new Grid(hei, wi);
-        utilGrid = new Grid(hei, wi);
-        clearGrid(mainGrid.getGameGrid());
-        observers = new ArrayList<Observator>();
-    }
-
-    public CellularAutomaton(Cell[][] grid) {
-        mainGrid = new Grid();
-        mainGrid.setGameGrid(grid);
-        utilGrid = new Grid(mainGrid.getHeight(), mainGrid.getWidth());
-        observers = new ArrayList<Observator>();
-    }
-
     public void resizeGameGrid(int hei, int wi) {
         mainGrid.resize(hei, wi);
         clearGrid(mainGrid.getGameGrid());
@@ -74,16 +54,15 @@ public abstract class CellularAutomaton implements Observable {
         clearGrid(utilGrid.getGameGrid());
     }
 
-    public void changeGrid() {
+    protected void changeGrid() {
         for (int i = 0; i < mainGrid.getHeight(); i++) {
             for (int j = 0; j < mainGrid.getWidth(); j++) {
                 mainGrid.setGameGridCell(i, j, utilGrid.getGameGridCell(i, j));
             }
         }
-        observers.toString();
     }
 
-    public void checkGrid() {
+    protected void checkGrid() {
         if (Arrays.deepEquals(mainGrid.getGameGrid(), utilGrid.getGameGrid())) {
             this.setIsGridChanging(false);
         } else {
@@ -91,34 +70,22 @@ public abstract class CellularAutomaton implements Observable {
         }
     }
 
-    protected void printToScreen() {
-        for (int i = 1; i < mainGrid.getHeight() - 1; i++) {
-            for (int j = 1; j < mainGrid.getWidth() - 1; j++) {
-                System.out.print(mainGrid.getGameGridCell(i, j) + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    public abstract void clearGrid(Cell[][] gridToClear);
+    protected abstract void clearGrid(Cell[][] gridToClear);
 
     public abstract void generate();
 
-    public abstract Cell cellState(int i, int j);
+    protected abstract Cell cellState(int i, int j);
 
-    /**
-     * @return the isGridChanging
-     */
     public boolean isIsGridChanging() {
         return this.isGridChanging;
     }
 
-    /**
-     * @param isGridChanging the isGridChanging to set
-     */
     public void setIsGridChanging(boolean isGridChanging) {
         this.isGridChanging = isGridChanging;
     }
-    
-   
+
+    public Grid getMainGrid() {
+        return mainGrid;
+    }
+
 }
